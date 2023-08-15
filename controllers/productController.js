451e1e4f548a963a66d6521b193b72c3bf91219c
jeanarm <nizigiarmel2015@gmail.com>
2023-08-15 21:17:23 +0200

    adding pagination to products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,17 @@
 
 const Product = require("../models/ProductModel")
+const recordsPerPage = require("../config/pagination")
+
 const getProducts = async (req, res, next) => {
 
   try {
+    const pageNum = Number(req.query.pageNum) || 1
+
     
-    const products = await Product.find({}).sort({name:"asc"}).orFail()
+    const products = await (await Product.find({}))
+    .skip(recordsPerPage * (pageNum - 1))
+    .sort({name:1})
+    .limit(recordsPerPage)
     res.json(products)
   } catch (error) {
     next(error)
