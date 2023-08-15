441e1ec39764e76b39caab0198e621d10afb12da
jeanarm <nizigiarmel2015@gmail.com>
2023-08-15 21:49:18 +0200

    adding dynamic sort

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,13 +6,28 @@ const getProducts = async (req, res, next) => {
 
   try {
     const pageNum = Number(req.query.pageNum) || 1
-
+    const totalProducts = await Product.countDocuments({})
     
-    const products = await (await Product.find({}))
+    //sort by nae,price, etc
+
+    let sort = {}
+
+    const sortOption = req.query.sort || ""
+  
+     if (sortOption){
+        let sortOpt = sortOption.split("_")
+        sort = {[sortOpt[0]]: Number(sortOpt[1])}
+     }
+
+    const products = await Product.find({})
     .skip(recordsPerPage * (pageNum - 1))
-    .sort({name:1})
+    .sort(sort)
     .limit(recordsPerPage)
-    res.json(products)
+    res.json({
+        products, 
+        pageNum, 
+        paginationLinksNumber:Math.ceil(totalProducts/recordsPerPage),
+    })
   } catch (error) {
     next(error)
     
